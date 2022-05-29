b98800e4e1860c226d43c5a201a17f227d084484
Ryu955 <ryuta_ishikawa@voyagegroup.com>
2022-05-29 22:51:27 +0900

    feat

--- src/content.js
+++ src/content.js
@@ -1,19 +1,19 @@
-window.addEventListener("load", main, false);
+window.addEventListener('load', main, false);
 
 function main(e) {
     const jsInitCheckTimer = setInterval(jsLoaded, 1000);
     function jsLoaded() {
-        if (document.getElementsByClassName( 'form-inline' ) != null) {
+        if (document.getElementsByClassName('form-inline') != null) {
             clearInterval(jsInitCheckTimer);
 
-            const btn = document.createElement("button");
-            btn.innerText= 'テーブルをコピー';
+            const btn = document.createElement('button');
+            btn.innerText=  'テーブルをコピー';
 
-            const form = document.getElementsByClassName( 'flex-fill' )[0];
+            const form = document.getElementsByClassName('flex-fill')[0];
             form.prepend(btn);
 
             btn.addEventListener('click', function(){
-                const table  = document.getElementsByClassName( 'table-condensed' )[0];
+                const table  = document.getElementsByClassName('table-condensed')[0];
                 const range = document.createRange();
                 range.selectNode(table);
                 const selection = window.getSelection();
