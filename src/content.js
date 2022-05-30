75db3063584eddd05b81818ad990318a0949e409
guri3 <daiki_oguri@voyagegroup.com>
2022-05-30 12:25:13 +0900

    feat: 書いてみた

--- src/content.js
+++ src/content.js
@@ -7,9 +7,12 @@ function main(e) {
             clearInterval(jsInitCheckTimer);
 
             const btn = document.createElement('button');
-            btn.innerText=  'テーブルをコピー';
+            btn.innerText =  'テーブルをコピー';
+            const mdBtn = document.createElement('button');
+            mdBtn.innerText = 'マークダウン形式でコピー'
 
             const form = document.getElementsByClassName('flex-fill')[0];
+            form.prepend(mdBtn);
             form.prepend(btn);
 
             btn.addEventListener('click', function(){
@@ -23,6 +26,60 @@ function main(e) {
 
                 selection.removeRange(range);
             });
+
+            mdBtn.addEventListener('click', function(){
+                const table  = document.getElementsByClassName('table-condensed')[0];
+                const range = document.createRange();
+                range.selectNode(table);
+                const selection = window.getSelection();
+                selection.removeAllRanges();
+                selection.addRange(range);
+                const text = selection.toString();
+                const mdText = text2md(text);
+                navigator.clipboard.writeText(mdText)
+                    .then(() => {
+                        console.log("Text copied to clipboard...")
+                    })
+                    .catch(err => {
+                        console.log('Something went wrong', err);
+                    })
+
+                selection.removeRange(range);
+            });
         }
     }
+}
+
+function text2md(text) {
+    let rows = text.split('\n');
+    // 先頭行が空文字
+    rows.shift();
+    let headers = rows.shift().split('\t');
+    // 最終項目が空文字
+    headers.pop()
+
+    // ヘッダー
+    const headersText = `|${headers.join('|')}|`;
+    const sepalatorText = `|${headers.map((_) => '---' ).join('|')}|`;
+
+    // 行
+    const columnNum = headers.length;
+    let rowText;
+    let count = 0;
+    let rowTexts = [];
+    rows.forEach(value => {
+        if (count === 0) {
+            rowText = '|';
+        }
+        rowText += value + '|';
+        if (count === columnNum - 1) {
+            rowTexts.push(rowText);
+            count = 0;
+            rowText = '';
+        } else {
+            count++;
+        }
+    });
+
+    return [headersText, sepalatorText, ...rowTexts].join('\n');
 }
